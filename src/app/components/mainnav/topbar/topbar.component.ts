8ffadde6eb90e5c4a7b48f7a0f40469505d9c940
Mohamed Ayman <az.mohamedayman12@gmail.com>
2019-10-12 17:00:46 +0200

    Working With Navbar

--- src/app/components/mainnav/topbar/topbar.component.ts
+++ src/app/components/mainnav/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ServiceService } from '../../../services/service.service';
 
 @Component({
   selector: 'app-topbar',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+  // Information About The Store
+  list = [
+    {
+      info: '565 Horiza Street - Philadilphia',
+      icon: 'fa fa-map-marker'
+    },
+    {
+      info: 'email@youremail.com',
+      icon: 'fa fa-home'
+    },
+    {
+      info: '123 977-654',
+      icon: 'fa fa-home'
+    }
+  ];
+
+  socialIcons = [
+    {name: 'fa fa-facebook'},
+    {name: 'fa fa-twitter'},
+    {name: 'fa fa-instagram'},
+  ];
+
+  collapse;
+
+  constructor(private serv: ServiceService) {}
+
+
 
   ngOnInit() {
+    this.collapse = this.serv.collapse;
+    this.serv.changeTopbar().subscribe((d) => {
+      this.collapse = d;
+    });
   }
-
 }
