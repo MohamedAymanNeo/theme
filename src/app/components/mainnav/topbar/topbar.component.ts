9d61093c4c139d606b82130e9b62bdd5a513f4e4
Mohamed Ayman <az.mohamedayman12@gmail.com>
2019-10-15 15:28:25 +0200

    add logo to navbar

--- src/app/components/mainnav/topbar/topbar.component.ts
+++ src/app/components/mainnav/topbar/topbar.component.ts
@@ -16,11 +16,7 @@ export class TopbarComponent implements OnInit {
     },
     {
       info: 'email@youremail.com',
-<<<<<<< HEAD
       icon: 'fa fa-envelope'
-=======
-      icon: 'fa fa-home'
->>>>>>> 37b4572d34d75651acb6a4495c5598dae160365a
     },
     {
       info: '123 977-654',
